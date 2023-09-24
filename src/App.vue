957a5df495a6a90785a76826c524027724f4fc40
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-09-24 11:53:12 +0200

    Add color switch

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,44 @@
 <script setup>
 import HeaderComponent from './components/HeaderComponent.vue';
+import AddNote from './components/AddNote.vue';
+import DisplayNotes from './components/DisplayNotes.vue';
+import { ref } from 'vue'
+
+const blueTheme = ref(true)
+const switchTheme = () => {
+  blueTheme.value = !blueTheme.value
+  console.log('test2' + blueTheme.value)
+}
 
 </script>
 
 <template>
-  <header>
-    <HeaderComponent/>
+  
+                        <header>
+                        <HeaderComponent @switch-theme="switchTheme"/>
+                        </header>
+                        <main>
+                        <AddNote :blue-theme="blueTheme.value"/>
+                        <DisplayNotes :blue-theme="blueTheme.value"/>
+                </main>
 
-    <div class="wrapper">
 
-    </div>
-  </header>
-
-  <main>
-  </main>
+    <!-- <div>
+<
+/div> -->
 </template>
 
 <style scoped>
+header {
+  margin-bottom: 150px;
+}
+
+.red {
+  background: #FF6666;
+  color: #FFEADD;
+  border: solid 2px #FCAEAE;
+}
+/*  *//*  */
 
 
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,22 +1,86 @@
 <script setup>
+import { ref } from 'vue';
+
+const blueTheme = ref(true)
+const emit = defineEmits(['switchTheme'])
+
+const switchTheme = () => {
+  blueTheme.value = !blueTheme.value
+  console.log('test' + blueTheme.value)
+  emit('switchTheme', blueTheme.value)
+}
+//
 
 
 </script>
 
 <template>
-  <header>
-    <img alt="Logo" class="logo" src="../assets/img/Notepad_icon.svg" />
-
-    <div class="wrapper">
+        <div class="header" :class="{ red: !blueTheme }">
+        <div>
+        <img alt="Logo" class="logo" src="../assets/img/Notepad_icon.svg" />
+      </div>
+    <div id="headline">
+    <h1 :class="{ red: !blueTheme }">Super-Notes</h1>
     </div>
-  </header>
+    <div class="button">
+    <button v-if="blueTheme" :class="{ red: blueTheme }" @click="switchTheme" >Switch theme</button>
+    <button v-else :class="{ red: blueTheme }" @click="switchTheme">Switch theme</button>
+    </div>
+    </div>
+    <!-- 
 
-  <main>
-  </main>
+       -->
 </template>
 
 <style scoped>
+.header {
+  position: fixed;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  /* background: v-bind('theme.background'); */
+  background: #4682A9;
+  gap: 20px;
+  padding: 10px;
+  top:0;
+  left: 0;
+  width: 100%;
+}
+#headline {
+  grid-column: 2 /span 2;
+  align-self: center;
+}
+
+.header h1 {
+   /* color: v-bind('theme.color'); */
+  color: #F6F4EB;
+  font-style: italic;
+}
 .logo {
   height: 125px;
 }
+
+button {
+background: #4682A9;
+color: #F6F4EB;
+border-radius: 5px;
+font-size: 20px;
+font-weight: 600;
+/* background: v-bind('theme.background'); */
+/* color: v-bind('theme.color'); */
+}
+
+.button {
+  grid-column: 7;
+  align-self: center;
+}
+
+.red {
+  background: #FF6666;
+  color: #FFEADD;
+  /* border: solid 2px #FCAEAE; */
+}
+
+/*  */
+/*  */
+
 </style>
