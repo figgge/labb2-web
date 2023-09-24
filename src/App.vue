549db7f1bd61f347ba0b37abd2e92f282377b677
Fredrik Kvarnåsen <102403209+figgge@users.noreply.github.com>
2023-09-24 08:53:12 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HeaderComponent from './components/HeaderComponent.vue';
+
+</script>
+
+<template>
+  <header>
+    <HeaderComponent/>
+
+    <div class="wrapper">
+
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <header>
+    <img alt="Logo" class="logo" src="../assets/img/Notepad_icon.svg" />
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+.logo {
+  height: 125px;
+}
+</style>
